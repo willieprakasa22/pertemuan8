53a639dbf2ba9d774f8362380c5d053686471282
hanafihp <fattaahherdansyah@students.amikom.ac.id>
2023-07-11 20:49:12 +0700

    first commit

---
+++ pertemuan_9/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace pertemuan_9
+{
+	public class Person
+	{
+		
+
+        public string Name { get; set; } 
+        public int Age { get; set; }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public void infoPerson()
+		{
+			Console.WriteLine("Nama saya {0}, dan umur saya {1}", Name, Age);
+		}
+    }
+
+
+	public class Karyawan : Person
+	{
+		public string idKaryawan { get; set; }
+        public string subjek { get; set; }
+
+        public Karyawan(string Name, int Age) : base(Name, Age)
+        {
+            
+
+        }
+
+        public void infoPerson(string subjek, string idKaryawan)
+        {
+            this.idKaryawan = idKaryawan;
+			this.subjek = subjek;
+            Console.WriteLine("saya disini sebagai {0}", this.subjek);
+            Console.WriteLine("Dengan id karyawan {0}", this.idKaryawan);
+        }
+    }
+}
+
---
+++ pertemuan_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using pertemuan_9;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person("Willie", 19);
+        person.infoPerson();
+        
+
+        Karyawan karyawan = new Karyawan("Sukoci", 22);
+        karyawan.infoPerson();
+        karyawan.infoPerson("pegawai", "22.11.4841");
+
+        Console.ReadLine();
+    }
+}
